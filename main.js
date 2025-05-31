6c1b2e64126d5bdafce339abb8507ff53347e400
m1lesmoral3s <orikreiback@me.com>
2025-05-31 18:53:31 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -19,25 +19,23 @@ const config = {
     }
 };
 
-let bird, cursors, obstacles, obstacleTimer, scoreText, bubble, bubbleText;
+let bird, cursors, obstacles, scoreText, bubble, bubbleText;
 let score = 0;
-let speechTimer;
 let frameToggle = true;
-let birdfred1, birdfred2;
 const fredLines = [
     "This bird needs a license!",
-    "You’re doing it! …don’t ruin it.",
-    "A puzzle?! I didn’t study!",
-    "Watch the cloud! THE CLOUD!",
-    "I'm not emotionally prepared for this!"
+    "I'm not emotionally prepared for this!",
+    "AHH! A balloon!",
+    "You better be good at this!",
+    "Don't drop me!"
 ];
 
 const game = new Phaser.Game(config);
 
 function preload () {
-    this.load.image('birdfred1', 'assets/images/birdfred.png');
-    this.load.image('birdfred2', 'assets/images/birdfred2.png');
-    this.load.image('obstacle', 'assets/images/obstacle.png');
+    this.load.image('birdfred1', 'birdfred.png');
+    this.load.image('birdfred2', 'birdfred2.png');
+    this.load.image('obstacle', 'obstacle.png');
 }
 
 function create () {
@@ -45,73 +43,72 @@ function create () {
     cursors = this.input.keyboard.createCursorKeys();
 
     obstacles = this.physics.add.group();
-    obstacleTimer = this.time.addEvent({
+
+    this.time.addEvent({
         delay: 1500,
-        callback: spawnObstacle,
+        callback: () => {
+            const y = Phaser.Math.Between(100, 540);
+            const obstacle = obstacles.create(500, y, 'obstacle');
+            obstacle.setVelocityX(-200);
+            obstacle.setImmovable(true);
+            obstacle.setScale(0.6);
+        },
         callbackScope: this,
         loop: true
     });
 
-    this.physics.add.collider(bird, obstacles, hitObstacle, null, this);
+    this.physics.add.collider(bird, obstacles, () => {
+        this.physics.pause();
+        bird.setTint(0xff0000);
+        bubble.setVisible(false);
+        bubbleText.setVisible(false);
+        scoreText.setText('Game Over!');
+    }, null, this);
 
     scoreText = this.add.text(10, 10, 'Score: 0', {
         fontSize: '20px',
         fill: '#000'
     });
 
-    bubble = this.add.rectangle(bird.x + 100, bird.y - 60, 200, 50, 0xffffff, 0.8)
-                     .setStrokeStyle(2, 0x000000)
-                     .setVisible(false);
-    bubbleText = this.add.text(bird.x + 60, bird.y - 80, '', {
+    bubble = this.add.graphics();
+    bubble.fillStyle(0xffffff, 0.9);
+    bubble.fillRoundedRect(0, 0, 200, 50, 10);
+    bubble.setScrollFactor(0);
+    bubble.setVisible(false);
+
+    bubbleText = this.add.text(0, 0, '', {
         fontSize: '14px',
         fill: '#000',
         wordWrap: { width: 180 }
     }).setVisible(false);
 
-    speechTimer = this.time.addEvent({
+    this.time.addEvent({
         delay: 4000,
-        callback: fredTalks,
+        callback: () => {
+            const line = Phaser.Utils.Array.GetRandom(fredLines);
+            bubble.setVisible(true);
+            bubble.x = bird.x + 60;
+            bubble.y = bird.y - 60;
+            bubbleText.setText(line);
+            bubbleText.setVisible(true);
+            bubbleText.x = bird.x + 70;
+            bubbleText.y = bird.y - 55;
+            this.time.delayedCall(2500, () => {
+                bubble.setVisible(false);
+                bubbleText.setVisible(false);
+            });
+        },
         callbackScope: this,
         loop: true
     });
 
     this.time.addEvent({
-        delay: 300,
-        callback: animateBird,
-        callbackScope: this,
+        delay: 250,
+        callback: () => {
+            frameToggle = !frameToggle;
+            bird.setTexture(frameToggle ? 'birdfred1' : 'birdfred2');
+        },
         loop: true
-    });
-}
-
-function animateBird() {
-    frameToggle = !frameToggle;
-    bird.setTexture(frameToggle ? 'birdfred1' : 'birdfred2');
-}
-
-function spawnObstacle () {
-    const y = Phaser.Math.Between(100, 540);
-    const obstacle = obstacles.create(500, y, 'obstacle');
-    obstacle.setVelocityX(-200);
-    obstacle.setImmovable(true);
-    obstacle.setScale(0.5);
-}
-
-function hitObstacle () {
-    this.physics.pause();
-    bird.setTint(0xff0000);
-    bubble.setVisible(false);
-    bubbleText.setVisible(false);
-    scoreText.setText('Game Over!');
-}
-
-function fredTalks() {
-    const line = Phaser.Utils.Array.GetRandom(fredLines);
-    bubble.setVisible(true);
-    bubbleText.setVisible(true);
-    bubbleText.setText(line);
-    this.time.delayedCall(2500, () => {
-        bubble.setVisible(false);
-        bubbleText.setVisible(false);
     });
 }
 
@@ -119,11 +116,6 @@ function update () {
     if (cursors.space.isDown) {
         bird.setVelocityY(-250);
     }
-
-    bubble.x = bird.x + 100;
-    bubble.y = bird.y - 60;
-    bubbleText.x = bird.x + 60;
-    bubbleText.y = bird.y - 80;
 
     score += 1;
     scoreText.setText('Score: ' + Math.floor(score / 60));
